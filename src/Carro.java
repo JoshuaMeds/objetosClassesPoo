012387b6f55956e0821b3d88ac80fad625aba8c8
Joshua Mendes <161641155+JoshuaMeds@users.noreply.github.com>
2024-09-19 08:56:54 -0300

    Add files via upload

---
+++ src/Carro.java
@@ -0,0 +1,23 @@
+public class Carro{
+    public String marca;
+    public String cor;
+    public int ano;
+    public String modelo;
+    public String chassi;
+
+    public void ligar(){
+        // implementação do código
+    }
+    public void desligar(){
+        // implementação do código
+    }
+    public void acelerar(){
+        // implementação do código
+    }
+    public void trocarMarcha (int marcha){
+        // ...
+    }
+    public void abrirVidro (int vidro){
+        // ...
+    }
+}
---
+++ src/Moto.java
@@ -0,0 +1,17 @@
+public class Moto {
+    public String cor;
+    public String modelo;
+    public String chassi;
+    public int ano;
+
+    public void ligar(){
+        // ...
+    }
+    public void desligar(){
+        //...
+    }
+    public void empinar(){
+        //...
+    }
+
+}
